47a2ada358ace96a4a659a1e967a453c31517fd3
Antonio <daza8843@gmail.com>
2025-11-12 23:57:28 -0500

    Cambio de dirección de escucha

--- src/server.ts
+++ src/server.ts
@@ -51,15 +51,17 @@ app.use((req, res, next) => {
  * Start the server if this module is the main entry point, or it is ran via PM2.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
-if (isMainModule(import.meta.url) || process.env['pm_id']) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
-    if (error) {
-      throw error;
-    }
-
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
+if (isMainModule(import.meta.url) || process.env['pm_id']) {  
+  const port = Number(process.env['PORT']) || 4000;  // Convertir a número  
+  const host = '0.0.0.0';  
+    
+  app.listen(port, host, (error) => {  
+    if (error) {  
+      throw error;  
+    }  
+  
+    console.log(`Node Express server listening on http://localhost:${port}`);  
+  });  
 }
 
 /**
